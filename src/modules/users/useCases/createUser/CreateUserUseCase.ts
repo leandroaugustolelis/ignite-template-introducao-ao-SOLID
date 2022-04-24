f9c3c36f4c99fae25db51a0929422532028f879d
Leandro Lelis <leandroaugustolelis@gmail.com>
2022-04-24 18:07:52 -0300

    SOLID project CRUD complete, creating user, listing all users, turning user into admin, etc

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,23 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userExists = this.usersRepository.findByEmail(email);
+
+        if (userExists) {
+            throw new Error("User email already exists");
+        }
+        const user = this.usersRepository.create({ email, name });
+
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
